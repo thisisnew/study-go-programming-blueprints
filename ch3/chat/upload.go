dccc51328203dad3e999cf231e6f85107554af37
thisisnew <thisisnew@naver.com>
2021-05-27 23:31:12 +0900

    3장 프로필 사진 구현의 세가지방법 (진행 중...어렵다...)

--- ch3/chat/upload.go
+++ ch3/chat/upload.go
@@ -8,7 +8,7 @@ import (
 )
 
 func uploadHandler(w http.ResponseWriter, req *http.Request) {
-	userId := req.FormValue("userid")
+	//userId := req.FormValue("userid")
 	file, header, err := req.FormFile("avatarFile")
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
@@ -19,7 +19,7 @@ func uploadHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	filename := path.Join("./avatars", userId+path.Ext(header.Filename))
+	filename := path.Join("./avatars", "userId"+path.Ext(header.Filename))
 	err = ioutil.WriteFile(filename, data, 0777)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
