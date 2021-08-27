1f001e3bf8a6a6a77aac09367d709f3f1965c572
thisisnew <thisisnew@naver.com>
2021-08-27 13:19:52 +0900

    1장 웹소켓을 이용한 채팅 애플리케이션 공부

--- ch1/chat/room.go
+++ ch1/chat/room.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"github.com/gorilla/websocket"
+	"log"
+	"net/http"
+)
+
 type room struct {
 	forward chan []byte
 	join    chan *client
@@ -21,4 +27,42 @@ func (r *room) run() {
 			}
 		}
 	}
+}
+
+func newRoom() *room {
+	return &room{
+		forward: make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+		clients: make(map[*client]bool),
+	}
+}
+
+const (
+	socketBufferSize  = 1024
+	messageBufferSize = 2048
+)
+
+var upgrader = &websocket.Upgrader{
+	ReadBufferSize:  socketBufferSize,
+	WriteBufferSize: messageBufferSize,
+}
+
+func (r *room) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	socket, err := upgrader.Upgrade(w, req, nil)
+	if err != nil {
+		log.Fatal("ServeHTTP:", err)
+		return
+	}
+
+	client := &client{
+		socket: socket,
+		send:   make(chan []byte, messageBufferSize),
+		room:   r,
+	}
+
+	r.join <- client
+	defer func() { r.leave <- client }()
+	go client.write()
+	client.read()
 }
