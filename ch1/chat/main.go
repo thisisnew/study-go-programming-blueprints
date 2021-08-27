08e10b343e5ae36fa1b279417dcf2595aad28dc6
thisisnew <thisisnew@naver.com>
2021-08-27 09:48:28 +0900

    1장 웹소켓을 이용한 채팅 애플리케이션 공부

--- ch1/chat/main.go
+++ ch1/chat/main.go
@@ -1,39 +1,24 @@
 package main
 
 import (
-	"flag"
 	"log"
 	"net/http"
-	"path/filepath"
-	"sync"
-	"text/template"
 )
 
-type templateHandler struct {
-	once     sync.Once
-	filename string
-	templ    *template.Template
-}
-
-func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	t.once.Do(func() {
-		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
-	})
-
-	t.templ.Execute(w, nil)
-}
-
 func main() {
-	var addr = flag.String("addr", ":8080", "The addr of the application")
-	flag.Parse()
-	r := newRoom()
-	http.Handle("/", &templateHandler{filename: "chat.html"})
-	http.Handle("/room", r)
-	go r.run()
-
-	log.Println("Starting web server on", *addr)
-
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`
+		<html>
+			<head>
+				<title>chat</title>
+			</head>
+			<body>
+				Let's chat!
+			</body>
+		</html>
+		`))
+	})
+	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
