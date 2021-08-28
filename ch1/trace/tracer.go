969cb2e2bca8d342067e73f397d48f1e5d3c419a
thisisnew <thisisnew@naver.com>
2021-08-28 22:57:30 +0900

    1장 웹소켓을 이용한 채팅 애플리케이션 공부. 추적시스템

--- ch1/trace/tracer.go
+++ ch1/trace/tracer.go
@@ -1,5 +1,23 @@
 package trace
 
+import (
+	"fmt"
+	"io"
+)
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	fmt.Fprint(t.out, a...)
+	fmt.Fprintln(t.out)
+}
+
 type Tracer interface {
 	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
 }
--- ch1/trace/tracer_test.go
+++ ch1/trace/tracer_test.go
@@ -1,9 +1,21 @@
 package trace
 
 import (
+	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	t.Error("We haven't written out test yet")
+
+	var buf bytes.Buffer
+	tracer := New(&buf)
+
+	if tracer == nil {
+		t.Error("Return from New should not be nil")
+	}
+	tracer.Trace("Hello trace package.")
+	if buf.String() != "Hello trace package.\n" {
+		t.Errorf("Trace should not write '%s'.", buf.String())
+	}
+
 }
