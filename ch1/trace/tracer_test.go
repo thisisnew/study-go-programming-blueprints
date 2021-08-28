153a06e3d66bb83e918e97259c468da82a286eb2
thisisnew <thisisnew@naver.com>
2021-08-28 22:39:26 +0900

    1장 웹소켓을 이용한 채팅 애플리케이션 공부. 추적시스템

--- ch1/trace/tracer_test.go
+++ ch1/trace/tracer_test.go
@@ -1,20 +1,9 @@
 package trace
 
 import (
-	"bytes"
 	"testing"
 )
 
 func TestNew(t *testing.T) {
-	var buf bytes.Buffer
-	tracer := New(&buf)
-
-	if tracer == nil {
-		t.Error("Return from New should not be nil")
-	} else {
-		tracer.Trace("Hello trace package")
-		if buf.String() != "Hello trace package.\n" {
-			t.Errorf("Trace should not write '%s'.", buf.String())
-		}
-	}
+	t.Error("We haven't written out test yet")
 }
