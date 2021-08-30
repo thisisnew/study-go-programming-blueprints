eae356cfdd00fd22178a04cc39d7680fbd4581f9
thisisnew <thisisnew@naver.com>
2021-08-30 12:52:49 +0900

    2장 사용자 계정 추가

--- ch2/chat/main.go
+++ ch2/chat/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"flag"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/facebook"
+	"github.com/stretchr/gomniauth/providers/github"
+	"github.com/stretchr/gomniauth/providers/google"
 	"log"
 	"net/http"
 	"path/filepath"
@@ -26,6 +30,13 @@ func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var addr = flag.String("addr", ":8080", "The addr of the application.")
 	flag.Parse()
+
+	gomniauth.SetSecurityKey("PUT YOUR AUTH KEY HERE")
+	gomniauth.WithProviders(
+		facebook.New("key", "secret", "http://localhost:8080/auth/callback/facebook"),
+		github.New("key", "secret", "http://localhost:8080/auth/callback/github"),
+		google.New("key", "secret", "http://localhost:8080/auth/callback/google"),
+	)
 
 	r := newRoom()
 	http.Handle("/", MustAuth(&templateHandler{filename: "chat.html"}))
