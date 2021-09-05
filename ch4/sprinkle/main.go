45efbe69df9625f1e48a0c83ed15b20b58370e62
thisisnew <thisisnew@naver.com>
2021-09-05 16:40:06 +0900

    4장 도메인 이름 검색용 커맨드라인 툴

---
+++ ch4/sprinkle/main.go
@@ -0,0 +1,14 @@
+package main
+
+const otherWord = "*"
+
+var transforms = []string{
+	otherWord,
+	otherWord + "app",
+	otherWord + "site",
+	otherWord + "time",
+	"get" + otherWord,
+	"go" + otherWord,
+	"lets" + otherWord,
+	otherWord + "hq",
+}
