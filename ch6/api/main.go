f9c6e5ac13f2bfcfce422f2e481d885a670b8316
thisisnew <thisisnew@naver.com>
2021-09-08 22:14:06 +0900

    6장 RESTful 데이터 웹 서비스 API를 통한 데이터와 기능 노출

--- ch6/api/main.go
+++ ch6/api/main.go
@@ -1,9 +1,35 @@
 package main
 
+import (
+	"context"
+	"net/http"
+)
+
 var contextKeyAPIKey = &contextKey{"api-key"}
 
 type contextKey struct {
 	name string
+}
+
+func APIkey(ctx context.Context) (string, bool) {
+	key, ok := ctx.Value(contextKeyAPIKey).(string)
+	return key, ok
+}
+
+func isValidAPIkey(key string) bool {
+	return key == "abc123"
+}
+
+func withAPIkey(fn http.HandlerFunc) http.HandlerFunc {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		key := request.URL.Query().Get("key")
+		if !isValidAPIkey(key) {
+			respondErr(writer, request, http.StatusUnauthorized, "invalid API key")
+			return
+		}
+		ctx := context.WithValue(request.Context(), contextKeyAPIKey, key)
+		fn(writer, request.WithContext(ctx))
+	}
 }
 
 func main() {
