c1696ee20f5de7e3752f5a9e2387764a0ab032b9
thisisnew <thisisnew@naver.com>
2021-09-20 18:06:40 +0900

    7장 랜덤 추천 웹서비스

--- ch7/meander/cost_level.go
+++ ch7/meander/cost_level.go
@@ -1,5 +1,10 @@
 package meander
 
+import (
+	"errors"
+	"strings"
+)
+
 type Cost int8
 
 const (
@@ -30,4 +35,24 @@ func (l Cost) String() string {
 
 func ParseCost(s string) Cost {
 	return costStrings[s]
+}
+
+type CostRange struct {
+	From Cost
+	To   Cost
+}
+
+func (r CostRange) String() string {
+	return r.From.String() + "..." + r.To.String()
+}
+
+func ParseCostRange(s string) (CostRange, error) {
+	var r CostRange
+	segs := strings.Split(s, "...")
+	if len(segs) != 2 {
+		return r, errors.New("invalid cost range")
+	}
+	r.From = ParseCost(segs[0])
+	r.To = ParseCost(segs[1])
+	return r, nil
 }
