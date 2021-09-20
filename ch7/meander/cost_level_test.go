1b0e6deb98461eb77652af80b1d589232adc2a29
thisisnew <thisisnew@naver.com>
2021-09-20 18:04:56 +0900

    7장 랜덤 추천 웹서비스

--- ch7/meander/cost_level_test.go
+++ ch7/meander/cost_level_test.go
@@ -15,5 +15,13 @@ func TestCostValues(t *testing.T) {
 	is.Equal(int(meander.Cost3), 3)
 	is.Equal(int(meander.Cost4), 4)
 	is.Equal(int(meander.Cost5), 5)
+}
 
+func TestCostString(t *testing.T) {
+	is := is.New(t)
+	is.Equal(meander.Cost1.String(), "$")
+	is.Equal(meander.Cost2.String(), "$$")
+	is.Equal(meander.Cost3.String(), "$$$")
+	is.Equal(meander.Cost4.String(), "$$$$")
+	is.Equal(meander.Cost5.String(), "$$$$$")
 }
