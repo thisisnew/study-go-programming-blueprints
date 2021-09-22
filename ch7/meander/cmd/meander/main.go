686dc8e51d9dc84faebf6e4128a643f34a79119d
thisisnew <thisisnew@naver.com>
2021-09-22 19:08:04 +0900

    7장 랜덤 추천 웹서비스

--- ch7/meander/cmd/meander/main.go
+++ ch7/meander/cmd/meander/main.go
@@ -3,13 +3,41 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
+	"strings"
 	"study-go-programming-blueprints/ch7/meander"
 )
 
 func main() {
+	meander.APIkey = ""
+
 	http.HandleFunc("/journeys", func(writer http.ResponseWriter, request *http.Request) {
 		respond(writer, request, meander.Journeys)
 	})
+	http.HandleFunc("/recommendations", cors(func(w http.ResponseWriter, r *http.Request) {
+		q := &meander.Query{
+			Journey: strings.Split(r.URL.Query().Get("journey"), "|"),
+		}
+		var err error
+		q.Lat, err = strconv.ParseFloat(r.URL.Query().Get("lat"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Lng, err = strconv.ParseFloat(r.URL.Query().Get("lng"), 64)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.Radius, err = strconv.Atoi(r.URL.Query().Get("radius"))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		q.CostRangeStr = r.URL.Query().Get("cost")
+		places := q.Run()
+		respond(w, r, places)
+	}))
 	http.ListenAndServe(":8080", http.DefaultServeMux)
 }
 
@@ -21,4 +49,11 @@ func respond(w http.ResponseWriter, r *http.Request, data []interface{}) error {
 	}
 
 	return json.NewEncoder(w).Encode(publicData)
+}
+
+func cors(f http.HandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		f(w, r)
+	}
 }
