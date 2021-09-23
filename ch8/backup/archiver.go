6e6cbb083d7a0e95158184005876e2fb984f5234
thisisnew <thisisnew@naver.com>
2021-09-23 21:42:03 +0900

    8장 파일시스템 백업

--- ch8/backup/archiver.go
+++ ch8/backup/archiver.go
@@ -1,8 +1,62 @@
 package backup
 
+import (
+	"archive/zip"
+	"io"
+	"os"
+	"path/filepath"
+)
+
 type Archiver interface {
 	Archiver(src, dest string) error
 }
 
 type zipper struct {
 }
+
+func (z *zipper) Archiver(src, dest string) error {
+	if err := os.MkdirAll(filepath.Dir(dest), 0777); err != nil {
+		return err
+	}
+
+	out, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer out.Close()
+
+	w := zip.NewWriter(out)
+	defer w.Close()
+
+	return filepath.Walk(src, func(path string, info os.FileInfo, err error) error {
+		if info.IsDir() {
+			return nil
+		}
+
+		if err != nil {
+			return err
+		}
+
+		in, err := os.Open(path)
+		if err != nil {
+			return err
+		}
+
+		defer in.Close()
+
+		f, err := w.Create(path)
+		if err != nil {
+			return err
+		}
+
+		_, err = io.Copy(f, in)
+		if err != nil {
+			return err
+		}
+		return nil
+
+	})
+}
+
+var ZIP Archiver = (*zipper)(nil)
